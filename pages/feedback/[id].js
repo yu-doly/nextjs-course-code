8f5589ebf8b63e7e7873071dfb209b80e106016d
yu-doly <yud0ly115@gmail.com>
2023-09-17 14:15:00 +0900

    Update
    Update around api

---
+++ pages/feedback/[id].js
@@ -0,0 +1,5 @@
+function FeedbackDetailPage () {
+  
+}
+
+export default FeedbackDetailPage
--- pages/index.js
+++ pages/index.js
@@ -1,34 +1,65 @@
-import { Fragment } from 'react';
-import Head from 'next/head';
+import { useRef, useState } from 'react';
 
-import FeaturedPosts from '../components/home-page/featured-posts';
-import Hero from '../components/home-page/hero';
-import { getFeaturedPosts } from '../lib/posts-util';
+function HomePage (props) {
+  const [feedbackItems, setFeedbackItems] = useState([]); 
 
-function HomePage(props) {
+  const emailInputRef = useRef();
+  const feedbackInputRef = useRef();
+
+  function submitFormHandler (event) {
+    event.preventDefault();
+    
+    const enteredEmail = emailInputRef.current.value;
+    const enteredFeedback = feedbackInputRef.current.value;
+
+    const reqBody = { email: enteredEmail, text: enteredFeedback };
+
+    fetch('/api/feedback', {
+      method: 'POST',
+      body: JSON.stringify(reqBody),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    }).then(res => res.json())
+      .then(data => console.log(data));
+  }
+
+  function loadFeedbackHandler () {
+    fetch('/api/feedback').then(res => res.json())
+      .then(data => setFeedbackItems(data.feedback));
+  }
+
   return (
-    <Fragment>
-      <Head>
-        <title>Max' Blog</title>
-        <meta
-          name='description'
-          content='I post about programming and web development.'
-        />
-      </Head>
-      <Hero />
-      <FeaturedPosts posts={props.posts} />
-    </Fragment>
+    <div>
+      <h1>The Home Page</h1>
+      <form onSubmit={submitFormHandler}>
+        <div>
+          <label htmlFor='email'>Your Email Address</label>
+          <input type='email' id='email' ref={emailInputRef} />
+        </div>
+        <div>
+          <label htmlFor='feedback'>Your Email Address</label>
+          <textarea id='feedback' rows="5" ref={feedbackInputRef} />
+        </div>
+        <button>Send Feedback</button>
+      </form>
+      <hr />
+      <button onClick={loadFeedbackHandler}>Load Feedback</button>
+      <ul>
+        {feedbackItems.map(item => <li key={item.id}>{item.text}</li>)}
+      </ul>
+    </div>
   );
 }
 
-export function getStaticProps() {
-  const featuredPosts = getFeaturedPosts();
+// export function getStaticProps() {
+//   const featuredPosts = getFeaturedPosts();
 
-  return {
-    props: {
-      posts: featuredPosts,
-    },
-  };
-}
+//   return {
+//     props: {
+//       posts: featuredPosts,
+//     },
+//   };
+// }
 
 export default HomePage;
